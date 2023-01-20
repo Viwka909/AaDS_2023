f8d9c77fec51fe3859d1ab8f1dd97fd7f8805001
badcat330 <alextvrus@gmail.com>
2023-01-20 22:35:58 +0300

    Add Lesson 2

--- Lesson1/Arrays.cs
+++ Lesson1/Arrays.cs
@@ -5,6 +5,7 @@ public class Array
     // Private section
     private int[] _container;
     private uint _count;
+    private bool _is_sorted = false;
 
     private void Resize()
     {
@@ -67,11 +68,120 @@ public class Array
 
     public void Remove(uint position)
     {
+        if (position < _count)
+        {
+            throw new Exception("Index out of range");
+        }
         
+        for (uint i = position; i < _count - 1; i++)
+        { 
+            _container[i] = _container[i + 1];
+        }
+
+        _count--;
+    }
+
+    public int this[uint index]
+    {
+        get
+        {
+            if (index >= _count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return _container[index];
+        }
+        
+        set
+        {
+            Insert(value, index);
+        }
+    }
+
+    public uint LinearSearch(int value) 
+    { 
+        for (uint i = 0; i < _count; i++) 
+        { 
+            if (value == _container[i]) 
+            { 
+                return i; 
+            } 
+        } 
+        
+        throw new Exception("Incorret value"); 
+    }
+
+    void Sort()
+    {
+        // Your Sort here!
+    }
+
+    public uint BinarySearchIter(int value)
+    {
+        uint start = 0;
+        uint end = _count;
+        uint mid;
+
+        if (!_is_sorted)
+        {
+            Sort();
+        }
+
+        while (start <= end)
+        {
+            mid = (start + end) / 2;
+
+            if (_container[mid] == value)
+            {
+                return mid;
+            }
+
+            if (value > _container[mid])
+            {
+                start = mid + 1;
+            }
+
+            if (value < _container[mid])
+            {
+                end = mid - 1;
+            }
+        }
+
+        throw new Exception("No such value");
+    }
+
+    public uint BinarySearch(int value)
+    {
+        if (!_is_sorted)
+            Sort();
+        return BinarySearch(value, 0, _count);
+    }
+    private uint BinarySearch(int value, uint start, uint end)
+    {
+        uint mid = (start + end) / 2;
+
+        if (start > end)
+            throw new Exception("No such value");
+
+        if (_container[mid] == value)
+            return mid;
+
+        if (_container[mid] > value)
+            return BinarySearch(value, start, mid - 1);
+        else
+            return BinarySearch(value, mid + 1, end);
     }
 
     public override string ToString()
     {
-        return base.ToString();
+        string buf = "[";
+        for (int i = 0; i < _count - 1; ++i)
+        {
+            buf += _container[i] + ", ";
+        }
+
+        buf += _container[_count - 1] + "]";
+        return buf;
     }
 }
---
+++ Lesson2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+uint Factarial(uint value)
+{
+    if (value == 0)
+        return 1;
+
+    return value * Factarial(value - 1);
+}
+
+uint FactarialIter(uint value)
+{
+    uint a = 1;
+
+    for(uint i = 1; i <= value; i++)
+        a *= i;
+    
+    return a;
+}
+
+uint N = 100000;
+
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+Factarial(N);
+stopWatch.Stop();
+
+TimeSpan ts = stopWatch.Elapsed;
+string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+    ts.Hours, ts.Minutes, ts.Seconds,
+    ts.Milliseconds / 10, ts.Ticks);
+Console.WriteLine("RunTime " + elapsedTime);
+
+stopWatch = new Stopwatch();
+stopWatch.Start();
+FactarialIter(N);
+stopWatch.Stop();
+
+ts = stopWatch.Elapsed;
+elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+    ts.Hours, ts.Minutes, ts.Seconds,
+    ts.Milliseconds / 10, ts.Ticks);
+Console.WriteLine("RunTime " + elapsedTime);
