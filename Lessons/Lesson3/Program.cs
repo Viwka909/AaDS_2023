055db4919b36362f9a20a5aef5c3b0b293315767
badcat330 <alextvrus@gmail.com>
2023-02-12 16:30:45 +0300

    Some more sorting.

--- Lessons/Lesson3/Program.cs
+++ Lessons/Lesson3/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using Lesson3;
 
-int[] test = {17, 15, 1, 76, 13, 14, 9, 48, 2, 21};
+int[] test = { 17, 15, 1, 76, 13, 14, 9, 48, 2, 21 };
 
 Sorting.SelectSort(ref test);
 
--- Lessons/Lesson3/Sorting.cs
+++ Lessons/Lesson3/Sorting.cs
@@ -23,22 +23,55 @@ public static class Sorting
         int max;
         for (int k = 0; k < arr.Length; k++)
         {
+            // find index min 
             max = 0;
             for (int i = 1; i < arr.Length - k; i++)
             {
                 if (arr[i] > arr[max])
                     max = i;
             }
-            
+
+            // swap
             int temp = arr[arr.Length - k - 1];
             arr[arr.Length - k - 1] = arr[max];
             arr[max] = temp;
         }
     }
 
-    public static void InsertSort(ref int[] arr)
-    {
+    private static void MergeSort(ref int[] arr, ref int[] buf, int l, int r) {
+        if (l >= r)
+            return;
+
         
+        int m = (l + r) / 2;
+        MergeSort(ref arr, ref buf, l, m);
+        MergeSort(ref arr, ref buf, m + 1, r);
+
+        // Merge
+        int k = l;
+        for (int i = l, j = m + 1; i <= m || j <= r; ) {
+            if (j > r || (i <= m && arr[i] < arr[j])) {
+                buf[k] = arr[i];
+                ++i;
+            }
+            else {
+                buf[k] = arr[j];
+                ++j;
+            }
+            ++k;
+        }
+        for (int i = l; i <= r; ++i) {
+            arr[i] = buf[i];
+        }
+        
+    }
+
+    public static void MergeSort(ref int[] arr) {
+        if (arr.Length == 0)
+            return;
+
+        int[] buf = new int[arr.Length];
+        MergeSort(ref arr, ref buf, 0, arr.Length - 1);
     }
 
 }
