f76407e078582cd41e518ec931979862f3c3f3d7
badcat330 <alextvrus@gmail.com>
2023-03-10 17:16:50 +0300

    Add Stacks

--- Lessons/Lesson7/Program.cs
+++ Lessons/Lesson7/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Generic;
+// See https://aka.ms/new-console-template for more information
 using System;
 int a = 4;
 Console.WriteLine(++a);
