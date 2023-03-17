12d422f990003dbeb6ad23002c5268039be215ed
badcat330 <alextvrus@gmail.com>
2023-03-17 11:05:12 +0300

    Better Stack code

--- Lessons/Lesson7/Stack.cs
+++ Lessons/Lesson7/Stack.cs
@@ -1,42 +1,59 @@
 using System.Collections.Generic;
-namespace Lesson7 {
-    class Stack {
+namespace Lesson7
+{
+    class Stack
+    {
+        class Node
+        {
+            public int value;
+            public Node? prev;
 
-        class Node {
-            int value;
-            Node prev;
-
-            public Node(int value, Node prev) {
+            public Node(int value, Node? prev)
+            {
                 this.value = value;
                 this.prev = prev;
             }
         }
 
-        private Node top;
+        private Node? top;
         private int size;
 
-        public Stack() {
+        public Stack()
+        {
             top = null;
             size = 0;
         }
 
-        public Stack(IEnumerable<int> collection) {
-            foreach(int buf in collection) {
+        public Stack(IEnumerable<int> collection)
+        {
+            foreach (int buf in collection)
+            {
                 Push(buf);
             }
         }
 
-        public void Push(int value) {
+        public void Push(int value)
+        {
             top = new Node(value, top);
             ++size;
         }
 
-        public int Top() {
-            return top.value;
+        public int Top()
+        {
+            if (top is Node node)
+            {
+                return node.value;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
         }
 
-        public int Pop() {
-            if (top is null) {
+        public int Pop()
+        {
+            if (top is null)
+            {
                 throw new System.Exception("Empty stack");
             }
 
@@ -46,9 +63,21 @@ namespace Lesson7 {
             return buf;
         }
 
-        public bool Empty() {
-            return top is null;
+        public bool Empty()
+        {
+            return size == 0;
         }
 
+        public override string ToString()
+        {
+            Node? buf = top;
+            string result = "";
+            while (buf != null)
+            {
+                result += $"{buf.value}\n";
+                buf = buf.prev;
+            }
+            return result;
+        }
     }
 }
