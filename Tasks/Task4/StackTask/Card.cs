ce09b66d493a74057ebc71b7fca7cc9f0df5a330
Viwka909 <vwko111@gmail.com>
2023-03-31 16:48:11 +0300

    kt4

--- Tasks/Task4/StackTask/Card.cs
+++ Tasks/Task4/StackTask/Card.cs
@@ -1,19 +1,55 @@
 namespace StackTask;
 
-public enum Element
+public enum CardElement
 {
-
+    Water,
+    Fire,
+    Earth,
+    Air,
+    Light,
+    Darkness
 }
 
 public enum CardType
 {
-
+    Creature,
+    Spell,
+    InstantSpell,
+    Territory
 }
 
 public class Card
 {
-    public Card(Element elem, CardType type, string text, int prior)
+    private CardElement elem;
+    private CardType type;
+    private string text = text;
+    private int prior = prior;
+    public CardElement Element
     {
-
+        get { return element;}
+    }
+    public CardType Type
+    {
+        get { return type;}
+    }
+    public string Text
+    {
+        get { return text;}
+    }
+    public int Prior
+    {
+        get { return prior;}
+    }
+    public Card(CardElement elem, CardType type, string text, int prior)
+    {
+        this.text = text;
+        this.prior = prior;
+        this.elem = elem;
+        this.type = type;
+    }
+    public override string ToString()
+    {
+        string result = $"\t\n{elem}\n\t{type}\n{text}\n";
+        return result;
     }
 }
--- Tasks/Task4/StackTask/CardStack.cs
+++ Tasks/Task4/StackTask/CardStack.cs
@@ -2,37 +2,111 @@
 
 public class CardStack
 {
+    class Node
+    {
+        public Card card;
+        public Node? prev;
+        public Node(Card card, Node? prev)
+        {
+            this.card = card;
+            this.prev = prev;
+        }
+    }
+    private int size;
+    private Node? top;
 
     public int Size
     {
         get
         {
-            throw new NotImplementedException();
+            return size;
         }
     }
 
     public void Push(Card card)
     {
-        throw new NotImplementedException();
+        if (top is null)
+        {
+            top = new Node(card, null);
+        }
+        else if (size == 100)
+        {
+            throw new System.Exception("Deck overfilled")
+        }
+        else
+        {
+            Node new_node = new(Node, top);
+            Node buf_node;
+            while (new_node.prev.card.Prior < new_node.card.Prior)
+            {
+                buf_node = new_node.prev;
+                new_node.prev = new_node.prev.prev;
+                buf_node.prev = new_node;
+            }
+        }
+
+        ++_size;
     }
 
     public Card Top()
     {
-        throw new NotImplementedException();
+        if (top is Card card)
+        {
+            return card;
+        }
+        else
+        {
+            throw new IndexOutOfRangeException();
+        }
     }
 
     public Card Pop()
     {
-        throw new NotImplementedException();
+        Node buf_node = top;
+        top = top.prev;
+        return buf_node;
     }
 
     public bool IsReadyForGame()
     {
-        throw new NotImplementedException();
+        if (size >= 30)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void Shuffle()
     {
-        throw new NotImplementedException();
+        var rng = new Random();
+
+        for (int i = 0; i < size - 1; ++i)
+        {
+            Node buf1 = top;
+            Node buf2 = top;
+            int gened = rng(i, size);
+            for (int k = 0; k < i; ++k)
+            {
+                buf1 = buf1.prev;
+            }
+            for (int k = 0; k < gened - 1; ++k)
+            {
+                buf2 = buf2.prev;
+            }
+            if (buf1 == top)
+            {
+                Node buf3 = buf2;
+                buf2.prev = buf1;
+                top = buf3;
+            }
+            else{
+                Node buf3 = buf2;
+                buf2.prev = buf1;
+                buf1.prev = buf3;
+            }
+        }
     }
 }
